3d7fc6cd095e5d01024ddd16a008a12bcbbfe3b6
Artem Los <arteml@kth.se>
2020-06-07 19:06:30 +0200

    Update Python script

--- retrieve_logs_last_month.py
+++ retrieve_logs_last_month.py
@@ -11,6 +11,7 @@ from licensing.models import *
 from licensing.methods import Key, Helpers
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from tools import *
 
@@ -20,6 +21,9 @@ logs = []
 
 ending_before=0
 
+"""
+Loading the data
+"""
 while True:
     res = Key.get_web_api_log(token=get_api_token(), order_by="Id descending", limit = 1000, ending_before=ending_before)
     
@@ -34,5 +38,17 @@ while True:
     ending_before = res[0][-1]["id"] 
     
 logs = pd.DataFrame(logs)
-
 logs = logs[logs["time"]>month_back]
+
+"""
+Some useful graphs
+"""
+
+key_freq = logs["key"].value_counts()
+key_freq[key_freq > sum(key_freq)*0.01].plot(kind='bar')
+plt.figure()
+
+logs["friendlyName"].value_counts().plot(kind="bar")
+plt.figure()
+
+logs["state"].value_counts().plot(kind="bar")
